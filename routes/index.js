86a22382d0cbe5acbb781eab7df8d8375bf37978
TodaBogdaWeb <todabogdaweb@gmail.com>
2021-07-01 10:59:50 +0200

    Commit15d

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.use('/', require("./home"));
 router.get('/ads.txt', (req,res) =>{
-    res.render("ads.txt");
+    res.render("ads");
 });
 module.exports = router;
